3269a66b5560e3a5b5861658990cc055c2891ad5
Sean Gillespie <sean@swgillespie.me>
2022-12-18 14:01:32 -0800

    bootstrap: fill in a bunch of escapes

--- tokenize.c
+++ tokenize.c
@@ -327,6 +327,28 @@ tokenize(void)
                   case '\\':
                     value |= '\\';
                     break;
+                  case '\?':
+                    value |= '\?';
+                    break;
+                  case 'a':
+                    value |= '\a';
+                    break;
+                  case 'b':
+                    value |= '\b';
+                    break;
+                  case 'f':
+                    value |= '\f';
+                    break;
+                  case 'r':
+                    value |= '\r';
+                    break;
+                  case 't':
+                    value |= '\t';
+                    break;
+                  case 'v':
+                    value |= '\v';
+                    break;
+
                   case 'n':
                     value |= '\n';
                     break;
